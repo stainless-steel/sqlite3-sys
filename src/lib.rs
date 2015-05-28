43d37f4ce3e575950988b50955cff43c04db774c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-28 17:40:46 -0400

    Add sqlite3_column_*

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate temporary;
 
 extern crate libc;
 
-use libc::{c_char, c_double, c_int, c_longlong, c_ulonglong, c_void};
+use libc::{c_char, c_double, c_int, c_longlong, c_uchar, c_ulonglong, c_void};
 
 #[repr(C)]
 pub struct sqlite3;
@@ -65,6 +65,10 @@ extern "C" {
                              del: Option<sqlite3_bind_callback>) -> c_int;
 
     pub fn sqlite3_close(db: *mut sqlite3) -> c_int;
+    pub fn sqlite3_column_double(stmt: *mut sqlite3_stmt, i: c_int) -> c_double;
+    pub fn sqlite3_column_int(stmt: *mut sqlite3_stmt, i: c_int) -> c_int;
+    pub fn sqlite3_column_int64(stmt: *mut sqlite3_stmt, i: c_int) -> sqlite3_int64;
+    pub fn sqlite3_column_text(stmt: *mut sqlite3_stmt, i: c_int) -> *const c_uchar;
     pub fn sqlite3_errmsg(db: *mut sqlite3) -> *const c_char;
 
     pub fn sqlite3_exec(db: *mut sqlite3, sql: *const c_char,
@@ -86,6 +90,7 @@ extern "C" {
 #[cfg(test)]
 mod tests {
     use std::ffi::{CStr, CString};
+    use std::ops::Deref;
     use temporary::Directory;
 
     macro_rules! ok(
@@ -97,6 +102,10 @@ mod tests {
     );
 
     macro_rules! c_str(
+        ($pointer:expr) => (CStr::from_ptr($pointer as *const _));
+    );
+
+    macro_rules! c_string(
         ($string:expr) => (ok!(CString::new($string)));
     );
 
@@ -105,53 +114,66 @@ mod tests {
         use libc::{c_char, c_int, c_void};
 
         open(|database| unsafe {
-            success!(::sqlite3_exec(database, c_str!(
+            success!(::sqlite3_exec(database, c_string!(
                 "CREATE TABLE `users` (id INTEGER, name VARCHAR(255), age REAL);"
             ).as_ptr(), None, 0 as *mut _, 0 as *mut _));
 
-            let mut statement = 0 as *mut _;
-            let mut tail = 0 as *const _;
-            success!(::sqlite3_prepare(database, c_str!(
-                "INSERT INTO `users` (id, name, age) VALUES (?, ?, ?);"
-            ).as_ptr(), -1, &mut statement, &mut tail));
+            {
+                let mut statement = 0 as *mut _;
+                let mut tail = 0 as *const _;
+                success!(::sqlite3_prepare(database, c_string!(
+                    "INSERT INTO `users` (id, name, age) VALUES (?, ?, ?);"
+                ).as_ptr(), -1, &mut statement, &mut tail));
 
-            let name = c_str!("Alice");
-            success!(::sqlite3_bind_int(statement, 1, 1));
-            success!(::sqlite3_bind_text(statement, 2, name.as_ptr(), -1, None));
-            success!(::sqlite3_bind_double(statement, 3, 20.99));
-            assert!(::sqlite3_step(statement) == ::SQLITE_DONE);
-            success!(::sqlite3_finalize(statement));
+                let name = c_string!("Alice");
+                success!(::sqlite3_bind_int(statement, 1, 1));
+                success!(::sqlite3_bind_text(statement, 2, name.as_ptr(), -1, None));
+                success!(::sqlite3_bind_double(statement, 3, 20.99));
+                assert!(::sqlite3_step(statement) == ::SQLITE_DONE);
 
-            extern fn list(done: *mut c_void, count: c_int, values: *mut *mut c_char,
-                           _: *mut *mut c_char) -> c_int {
-
-                unsafe {
-                    use std::ops::Deref;
-                    assert!(count == 3);
-
-                    assert!(CStr::from_ptr(*values) ==
-                            ok!(CString::new("1")).deref());
-
-                    assert!(CStr::from_ptr(*values.offset(1)) ==
-                            ok!(CString::new("Alice")).deref());
-
-                    assert!(CStr::from_ptr(*values.offset(2)) ==
-                            ok!(CString::new("20.99")).deref());
-
-                    *(done as *mut bool) = true;
-                }
-
-                0
+                success!(::sqlite3_finalize(statement));
             }
 
-            let mut done = false;
+            {
+                let mut done = false;
+                success!(::sqlite3_exec(database, c_string!(
+                    "SELECT * FROM `users`;"
+                ).as_ptr(), Some(list), &mut done as *mut _ as *mut _, 0 as *mut _));
+                assert!(done);
+            }
 
-            success!(::sqlite3_exec(database, c_str!(
-                "SELECT * FROM `users`;"
-            ).as_ptr(), Some(list), &mut done as *mut _ as *mut _, 0 as *mut _));
+            {
+                let mut statement = 0 as *mut _;
+                let mut tail = 0 as *const _;
+                success!(::sqlite3_prepare(database, c_string!(
+                    "SELECT * FROM `users`;"
+                ).as_ptr(), -1, &mut statement, &mut tail));
 
-            assert!(done);
+                assert!(::sqlite3_step(statement) == ::SQLITE_ROW);
+                assert!(::sqlite3_column_int(statement, 0) == 1);
+                assert!(c_str!(::sqlite3_column_text(statement, 1)) == c_string!("Alice").deref());
+                assert!(::sqlite3_column_double(statement, 2) == 20.99);
+
+                assert!(::sqlite3_step(statement) == ::SQLITE_DONE);
+
+                success!(::sqlite3_finalize(statement));
+            }
         });
+
+        extern fn list(done: *mut c_void, count: c_int, values: *mut *mut c_char,
+                       _: *mut *mut c_char) -> c_int {
+
+            unsafe {
+                assert!(count == 3);
+
+                assert!(c_str!(*values) == c_string!("1").deref());
+                assert!(c_str!(*values.offset(1)) == c_string!("Alice").deref());
+                assert!(c_str!(*values.offset(2)) == c_string!("20.99").deref());
+
+                *(done as *mut bool) = true;
+            }
+            0
+        }
     }
 
     fn open<F>(mut code: F) where F: FnMut(*mut ::sqlite3) {
@@ -167,7 +189,7 @@ mod tests {
     fn setup() -> (CString, Directory) {
         let directory = ok!(Directory::new("sqlite-sys"));
         let path = directory.path().join("database.sqlite3");
-        let path = c_str!(ok!(path.to_str()));
+        let path = c_string!(ok!(path.to_str()));
         (path, directory)
     }
 }
