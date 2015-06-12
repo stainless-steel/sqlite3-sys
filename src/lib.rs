717d5c4d1a5bce6fc0db4bad471b857c12c34119
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-12 14:14:00 -0400

    Add extended result codes and flags for open operations

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ extern crate libc;
 
 use libc::{c_char, c_double, c_int, c_longlong, c_uchar, c_ulonglong, c_void};
 
+mod constants;
+pub use constants::*;
+
 #[repr(C)]
 pub struct sqlite3;
 
@@ -15,38 +18,6 @@ pub type sqlite_uint64 = c_ulonglong;
 
 pub type sqlite3_int64 = sqlite_int64;
 pub type sqlite3_uint64 = sqlite_uint64;
-
-pub const SQLITE_OK: c_int = 0;
-pub const SQLITE_ERROR: c_int = 1;
-pub const SQLITE_INTERNAL: c_int = 2;
-pub const SQLITE_PERM: c_int = 3;
-pub const SQLITE_ABORT: c_int = 4;
-pub const SQLITE_BUSY: c_int = 5;
-pub const SQLITE_LOCKED: c_int = 6;
-pub const SQLITE_NOMEM: c_int = 7;
-pub const SQLITE_READONLY: c_int = 8;
-pub const SQLITE_INTERRUPT: c_int = 9;
-pub const SQLITE_IOERR: c_int = 10;
-pub const SQLITE_CORRUPT: c_int = 11;
-pub const SQLITE_NOTFOUND: c_int = 12;
-pub const SQLITE_FULL: c_int = 13;
-pub const SQLITE_CANTOPEN: c_int = 14;
-pub const SQLITE_PROTOCOL: c_int = 15;
-pub const SQLITE_EMPTY: c_int = 16;
-pub const SQLITE_SCHEMA: c_int = 17;
-pub const SQLITE_TOOBIG: c_int = 18;
-pub const SQLITE_CONSTRAINT: c_int = 19;
-pub const SQLITE_MISMATCH: c_int = 20;
-pub const SQLITE_MISUSE: c_int = 21;
-pub const SQLITE_NOLFS: c_int = 22;
-pub const SQLITE_AUTH: c_int = 23;
-pub const SQLITE_FORMAT: c_int = 24;
-pub const SQLITE_RANGE: c_int = 25;
-pub const SQLITE_NOTADB: c_int = 26;
-pub const SQLITE_NOTICE: c_int = 27;
-pub const SQLITE_WARNING: c_int = 28;
-pub const SQLITE_ROW: c_int = 100;
-pub const SQLITE_DONE: c_int = 101;
 
 pub type sqlite3_bind_callback = extern fn(*mut c_void);
 pub type sqlite3_busy_callback = extern fn(*mut c_void, c_int) -> c_int;
